29fd73163066df60603925956a937dce11490873
Zain Haque <zainulhaque@rockvillegroup.com>
2022-02-23 14:58:28 +0500

    cleaned up response

--- property-service/src/main/java/com/example/propertyservice/model/BookingModel.java
+++ property-service/src/main/java/com/example/propertyservice/model/BookingModel.java
@@ -1,8 +1,6 @@
 package com.example.propertyservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- property-service/src/main/java/com/example/propertyservice/model/ReviewModel.java
+++ property-service/src/main/java/com/example/propertyservice/model/ReviewModel.java
@@ -1,10 +1,6 @@
 package com.example.propertyservice.model;
 
-import com.example.propertyservice.model.BookingModel;
-import com.example.propertyservice.model.PropertyModel;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
